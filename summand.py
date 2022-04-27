dd511f814ea372797b8e8c5914753aa0ccbaf909
yuxuanjerrychen01 <yuxuanjerrychen01@163.com>
2022-04-27 02:38:59 -0400

    finished upper_summand, kh function

--- summand.py
+++ summand.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sympy as sp
+import shift
 
 
 def poly_helper(poly, q, t):
@@ -37,8 +38,8 @@ def VaE(a, E):
     exceptional = 0
     for i in range(len(a)):
         aprime.append(a[i] - E[i])
-        exceptional += E[i] * (1 + q**2) * (q**(4 * i - 4)) * t**(2 * i - 2)
-    knights_move = sp.expand((1 + q**4 * t) * V_tilde(aprime)[0])
+        exceptional += E[i] * (1 + q ** 2) * (q ** (4 * i - 4)) * t ** (2 * i - 2)
+    knights_move = sp.expand((1 + q ** 4 * t) * V_tilde(aprime)[0])
     output = sp.expand(knights_move * exceptional)
     ppp = poly_helper(output, q, t)
     return output, ppp
@@ -92,14 +93,14 @@ def special_sequence(sequence_type, k):
 
 def lower_summand(l, m, n):
     if (m != l) and (l % 2 == 1):
-        sequence = special_sequence("a", l-1)
+        sequence = special_sequence("a", l - 1)
         sequence += [int((l - 1) / 2)] * 2
-        sequence += special_sequence("a", l-1)[::-1]
+        sequence += special_sequence("a", l - 1)[::-1]
         Lower_sum = Va(sequence)
         return sequence, Lower_sum
 
     if (m != l) and (l % 2 == 0):
-        sequence = [1] + special_sequence("c", l-4)
+        sequence = [1] + special_sequence("c", l - 4)
         sequence += [int((l - 2) / 2)] * 3 + [int(l / 2)]
         sequence += special_sequence("b", l)[::-1]
         E = [1] + [0] * (len(sequence) - 1)
@@ -107,15 +108,15 @@ def lower_summand(l, m, n):
         return sequence, Lower_sum
 
     if (m == l) and (l % 2 == 1):
-        sequence = special_sequence("a", l-1) + [int((l - 1) / 2)] * 2
-        sequence += special_sequence("a", l-1)[::-1]
+        sequence = special_sequence("a", l - 1) + [int((l - 1) / 2)] * 2
+        sequence += special_sequence("a", l - 1)[::-1]
         sequence += [0] * (n - l) + [1]
         E = [0] * (len(sequence) - 1) + [1]
         Lower_sum = VaE(sequence, E)
         return sequence, Lower_sum
 
     if (m == l) and (l % 2 == 0) and (n % 2 == 1):
-        sequence = [1] + special_sequence("c", l-4)
+        sequence = [1] + special_sequence("c", l - 4)
         sequence += [int((l - 2) / 2)] * 3 + [int(l / 2)]
         sequence += special_sequence("b", l)[::-1] + [0, 1] * (int((n - l - 1) / 2))
         E = [1] + [0] * (len(sequence) - 1)
@@ -123,7 +124,7 @@ def lower_summand(l, m, n):
         return sequence, Lower_sum
 
     if (m == l) and (l % 2 == 0) and (n % 2 == 0) and (n != l):
-        sequence = [1] + special_sequence("c", l-4)
+        sequence = [1] + special_sequence("c", l - 4)
         sequence += [int((l - 2) / 2)] * 3 + [int(l / 2)]
         sequence += special_sequence("b", l)[::-1] + [0, 1] * (int((n - l) / 2))
         E = [1] + [0] * (len(sequence) - 2) + [1]
@@ -131,7 +132,7 @@ def lower_summand(l, m, n):
         return sequence, Lower_sum
 
     if (m == l) and (l % 2 == 0) and (n == l):
-        sequence = [1] + special_sequence("c", l-4)
+        sequence = [1] + special_sequence("c", l - 4)
         sequence += [int((l - 2) / 2)] * 3 + [int(l / 2)]
         temp = special_sequence("b", l)[0]
         temp[0] += 1
@@ -141,12 +142,111 @@ def lower_summand(l, m, n):
         return sequence, Lower_sum
 
 
+def upper_summand(l, m, n):
+    g = m - l
+    h = n - m
+
+    if (l % 2 == 1) and (m % 2 == 1) and (n % 2 == 1):
+        sequence = special_sequence("a", g)
+        sequence += [int(g / 2)] * h
+        sequence += special_sequence("a", g)[::-1]
+        E = [0] * (len(sequence) - 1) + [1]
+        upper_sum = VaE(sequence, E)
+        return sequence, upper_sum
+
+    if (l % 2 == 1) and (m % 2 == 1) and (n % 2 == 0):
+        sequence = special_sequence("a", g)
+        sequence += [int(g / 2)] * (h - 1)
+        sequence += special_sequence("c", g)[::-1]
+        E = [0] * (g + h - 1) + [1] + [0] * (g - 1)
+        upper_sum = VaE(sequence, E)
+        return sequence, upper_sum
+
+    if (l % 2 == 1) and (m % 2 == 0) and (n % 2 == 1):
+        sequence = special_sequence("a", g - 1)
+        sequence += [int((g + 1) / 2), int((g - 1) / 2)] * (int((h + 1) / 2))
+        sequence += special_sequence("c", g - 1)[::-1]
+        E = [0] * (g - 1) + [1] + [0] * h + [0] * (g - 1)
+        upper_sum = VaE(sequence, E)
+        return sequence, upper_sum
+
+    if (l % 2 == 1) and (m % 2 == 0) and (n % 2 == 0):
+        sequence = special_sequence("a", g - 1)
+        sequence += [int((g + 1) / 2), int((g - 1) / 2)] * (int(h / 2)) + [int((g + 1) / 2)]
+        sequence += special_sequence("c", g - 1)[::-1]
+        if h == 0:
+            E = [0] * (g - 1) + [2] + [0] * (g - 1)
+            upper_sum = VaE(sequence, E)
+            return sequence, upper_sum
+        else:
+            E = [0] * (g - 1) + [1] + [0] * (h - 1) + [1] + [0] * (g - 2)
+            upper_sum = VaE(sequence, E)
+            return sequence, upper_sum
+
+    if (l % 2 == 0) and (m % 2 == 1) and (n % 2 == 1):
+        sequence = special_sequence("b", g + 1)
+        sequence += [int((g + 1) / 2), int((g - 1) / 2)] * (int(h / 2))
+        sequence += special_sequence("c", g - 1)[::-1]
+        upper_sum = Va(sequence)
+        return sequence, upper_sum
+
+    if (l % 2 == 0) and (m % 2 == 1) and (n % 2 == 0):
+        sequence = special_sequence("b", g + 1)
+        sequence += [int((g + 1) / 2), int((g - 1) / 2)] * (int((h - 1) / 2))
+        sequence += special_sequence("c", g)[::-1]
+        E = [0] * (g + 1) + [0] * (h - 1) + [1] + [0] * (g - 1)
+        upper_sum = VaE(sequence, E)
+        return sequence, upper_sum
+
+    if (l % 2 == 0) and (m % 2 == 0) and (n % 2 == 1):
+        sequence = special_sequence("b", g)
+        if g == 0:
+            sequence += [1, 0] * (int((h + 1) / 2))
+            sequence += special_sequence("c", 0)[::-1]
+        else:
+            sequence += [int((g + 2) / 2), int((g - 2) / 2)] * (int((h + 1) / 2))
+            sequence += special_sequence("c", g - 1)[::-1]
+        E = [0] * g + [1] + [0] * h + [0] * (g - 1)
+        upper_sum = VaE(sequence, E)
+        return sequence, upper_sum
+
+    if (l % 2 == 0) and (m % 2 == 0) and (n % 2 == 0):
+        sequence = special_sequence("b", g)
+        if g == 0:
+            sequence += [1, 0] * (int(h / 2)) + [1]
+            sequence += special_sequence("a", 0)[::-1]
+        else:
+            sequence += [int((g + 2) / 2), int((g - 2) / 2)] * (int(h / 2)) + [int((g + 2) / 2)]
+            sequence += special_sequence("a", g)[::-1]
+        if h == 0:
+            E = [0] * g + [2] + [0] * (g - 1) + [1]
+            upper_sum = VaE(sequence, E)
+            return sequence, upper_sum
+        else:
+            E = [0] * g + [1] + [0] * (h - 1) + [1] + [0] * (g - 1) + [1]
+            upper_sum = VaE(sequence, E)
+            return sequence, upper_sum
+
+
+def Kh(l, m, n):
+    if 2 <= l <= m <= n:
+        q, t = sp.symbols('q t')
+        s_l, t_l, s_u, t_u = shift.shift_table(l, m, n)
+        part1 = (q ** int(s_l)) * (t ** int(t_l)) * lower_summand(l, m, n)[1][0]
+        part2 = (q ** int(s_u)) * (t ** int(t_u)) * upper_summand(l, m, n)[1][0]
+        homology_poly = sp.expand(part1 + part2)
+        q_power, t_power, coeffs = poly_helper(homology_poly, q, t)
+
+        return q_power, t_power, coeffs
+    else:
+        return -404, -404, -404
+
 
 # vec = [1, 2, 1]
 # # print(V_tilde(vec, q, )[0])
 # print(VaE(vec, [0,1,0])[1])
 
 # print(special_sequence("c", 10))
-print(lower_summand(2,3,4)[0])
+# print(lower_summand(2,3,4)[0])
 print(lower_summand(2,3,4)[1])
-
+print(Kh(2, 3, 4))
